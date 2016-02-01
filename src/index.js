9b385af13160b88884b993aeb98135ca4f47fe64
Greg Collett <gregory.collett@live.co.uk>
2016-02-01 18:32:22 +0000

    fix routes case sensitivity - through warning at console.log eslint

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import Routes from './Routes';
+import routes from './routes';
 import store from './store';
 
 // this line throws an eslint error but we need to import otherwise the
 // indexeddb is never instantiated... TODO find a better way to manage this
 import db from './utils/Database';
 
+console.log(db);
+
 import AppContainer from './containers/AppContainer';
 
 render(
   <Provider store={store}>
     <AppContainer>
-      <Routes/>
+      <routes/>
     </AppContainer>
   </Provider>,
   document.getElementById('app')
