bed53a8999be005138135c4d82d8c8dcd730fafc
Greg Collett <gregory.collett@live.co.uk>
2016-02-01 18:08:28 +0000

    add babel-eslint to make eslint stop moaning. add comment to index about instantiating indexeddb

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import Routes from './Routes';
 import store from './store';
 
+// this line throws an eslint error but we need to import otherwise the
+// indexeddb is never instantiated... TODO find a better way to manage this
 import db from './utils/Database';
 
 import AppContainer from './containers/AppContainer';
