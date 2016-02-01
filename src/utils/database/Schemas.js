c4ca117f109b72e29c084a64ac2395c0fad6c151
Greg Collett <gregory.collett@live.co.uk>
2016-02-01 18:35:36 +0000

    update db schemas file

--- src/utils/database/Schemas.js
+++ src/utils/database/Schemas.js
@@ -1,5 +1,12 @@
+/**
+ * Setup IndexedDB schemas (as per dexie documentation)
+ * @param  {Dexie} db
+ * @return {Dexie} returns the db initially passed to the function
+ */
 export default function(db) {
-  // setup your schemas here.
+  // Setup your schemas here
+  // In theory we could move each schema in to its own module
+  // TODO: create an example showing the above
   db.app.defineClass({
     type: String,
     value: String
@@ -8,5 +15,6 @@ export default function(db) {
   // example insert/update
   db.app.put({type: 'name', value: 'REACT ES6 STARTER KIT...'});
 
+  // return the db
   return db;
 }
