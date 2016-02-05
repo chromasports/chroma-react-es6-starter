0de7a3127dc20a5e5609065fef0d87dea1722e7a
Greg Collett <gregory.collett@live.co.uk>
2016-02-05 11:38:39 +0000

    add some comments - update readme

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // hook babel - from this point onwards we can use esNEXT syntax :D
 require('babel-core/register');
 require("babel-polyfill");
-// require the dev server
+
+// require the backend server
 require('./src/server');
+// require the webpack client dev server
 require('./tasks/webpack/dev-server');
